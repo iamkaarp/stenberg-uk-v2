5fdf312e386ba748cc22b5a96d0049ed23c552c5
Joakim <joakim.stenberg88@gmail.com>
2020-09-21 18:20:57 +0200

    removed unused stuff and adding my icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,15 @@
 <template>
     <div class="container mx-auto">
         <face />
-        <what-i-do />
-        <fun-facts />
     </div>
 </template>
 
 <script>
 import Face from '@/components/Face.vue';
-import WhatIDo from '@/components/WhatIDo.vue';
-import FunFacts from '@/components/FunFacts.vue';
 
 export default {
     components: {
         Face,
-        WhatIDo,
-        FunFacts,
     },
 };
 </script>
